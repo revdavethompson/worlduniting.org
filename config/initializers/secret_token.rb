703053d1a17e5670725b450f6eb93d2acd4ce50f
dathompson <da@dathompson.com>
2012-02-15 15:24:33 -0800

    initial import of base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorldunitingOrg::Application.config.secret_token = '3bc86e84144a5e2d5abdc6cfbb2deae263c7a8a1eafbf51478f2d142edd0ea350fa98426ffd7cc006dbe8d451207fa548cd27372ffd7fbba12a4387ec62c43a8'
